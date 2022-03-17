b8e57e4b39bf3475748f1c797e2acbf6869cfa1e
gooddream <alwaysmrp@vip.qq.com>
2022-03-18 04:44:00 +0800

    new script

---
+++ feishu_copy.js
@@ -0,0 +1,15 @@
+(function name(ele,filename='selected_text.txt') {
+    const blob = new Blob([ele.innerText], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 0);
+})(document.body)
